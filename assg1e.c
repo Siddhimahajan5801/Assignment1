90c025f24eaeb2b73ac57e348268b22cc12b6025
Siddhimahajan5801 <110876496+Siddhimahajan5801@users.noreply.github.com>
2022-08-09 18:49:44 +0530

    Update assg1e.c

--- assg1e.c
+++ assg1e.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
-#include<string.h>
 
 int main(){
-    char a[50];
     int l;
 
-    printf("Enter the string whose length is to be calculated:\n");
-    gets(a);
-
-    l=strlen(a);
-    printf("Length of the string is %d",l);
+    l=printf("hello\n");
+    printf("Length of the given string is %d",l);
     return 0;
 }
